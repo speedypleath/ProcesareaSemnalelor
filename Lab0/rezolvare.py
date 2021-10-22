f0207b70867acbfd6db3f63e4490866892fa5538
speedypleath <gheorgheandrei13@gmail.com>
2021-10-22 03:41:28 +0300

    Lab1

---
+++ Lab0/rezolvare.py
@@ -0,0 +1,24 @@
+import numpy as np
+import matplotlib
+import matplotlib.pyplot as plt
+matplotlib.use('TkAgg')
+def sine(amplitude, frequency, phase, time):
+    return amplitude * np.sin(2 * np.pi * frequency * time + phase)
+
+amplitude = 1
+frequency = 2
+phase = np.pi
+
+n_samples = 60
+time_of_view = 2
+
+atime = np.linspace(0, time_of_view, int(10e5))
+time = np.linspace(0, time_of_view, int(n_samples))
+
+asignal = sine(amplitude, frequency, phase, atime)
+signal = sine(amplitude, frequency, phase, time)
+
+plt.plot(atime, asignal)
+plt.grid(True)
+plt.stem(time, signal)
+plt.show()
